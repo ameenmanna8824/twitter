6c8cd4a4331a2586f50c9791c994da5abe98cef6
ameenmanna8824 <52666434+ameenmanna8824@users.noreply.github.com>
2021-01-28 10:45:59 +0530

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -1,10 +1,10 @@
 import tweepy
 import os
 
-consumer_key = os.getenv('consumer_key)
-consumer_secret = os.getenv('consumer_secret)
-access_token = os.getenv('access_token)
-access_token_secret = os.getenv('access_token_secret)
+consumer_key = os.getenv('consumer_key')
+consumer_secret = os.getenv('consumer_secret')
+access_token = os.getenv('access_token')
+access_token_secret = os.getenv('access_token_secret')
 
 auth =tweepy.OAuthHandler(consumer_key,consumer_secret)
 auth.set_access_token(access_token,access_token_secret)
